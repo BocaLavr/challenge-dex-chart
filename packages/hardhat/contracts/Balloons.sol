3b123aed6aae1873bf69445e39b9a847eee280c7
BocaLavr <108471752+BocaLavr@users.noreply.github.com>
2023-12-06 11:33:36 +0700

    Update Balloons.sol

--- packages/hardhat/contracts/Balloons.sol
+++ packages/hardhat/contracts/Balloons.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Balloons is ERC20 {
     constructor() ERC20("Charisma", "RIZZ") {
-        _mint(msg.sender, 1000 ether); // mints 1000 balloons!
+        _mint(msg.sender, 7777 ether); // mints 7777 balloons!
     }
 }
